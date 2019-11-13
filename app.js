e35616a7a112896f29c224d2f0fbd5463aaf9e40
romiinavega <romiivega3005@gmail.com>
2019-11-13 15:44:34 -0700

    d,

---
+++ app.js
@@ -0,0 +1,33 @@
+const namee = document.getElementById('name1');
+const secondnamee = document.getElementById('secondname1');
+const lastnamee = document.getElementById('lastname1');
+const agee = document.getElementById('age1');
+const photoo = document.getElementById('photo1');
+const descriptioon = document.getElementById('description1')
+const occupationn = document.getElementById('occupation1');
+const birthdatee = document.getElementById('birthdate1');
+
+const apiUrl = 'https://localhost:5001/simpsons/character'
+
+const apiResponse = async url => {
+    const response = await fetch(`${apiUrl}/3`)
+    const data = await response.json();
+    console.log(data);
+
+    const {name, secondName, lastName, age, description, occupation, birthdate } = data;
+    const imgpic = data.photo;
+
+
+
+    photoo.src = imgpic;
+    agee.innerText = age;
+    namee.innerText = name;
+    secondnamee.innerText = secondName;
+    lastnamee.innerText = lastName;
+    descriptioon.innerText = description;
+    occupationn.innerText = occupation;
+    birthdatee.innerText = birthdate;
+  
+};
+
+apiResponse(apiUrl);
